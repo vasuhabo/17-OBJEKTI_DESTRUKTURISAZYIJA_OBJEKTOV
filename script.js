05b2ea58a6d6e0b38ad563fb371f61c75a64315a
sahumo <7523460@gmail.com>
2021-09-21 02:05:26 +0300

    СЧЕТЧИК  КОЛ-ВА  СВОЙСТВ ВСЕХ  КАТЕГОРИЙ  БЕЗ  ПОДКАТЕГОРИЙ  ОБЬЕКТА ВЕРХНИЙ  УРОВЕНЬ

--- script.js
+++ script.js
@@ -12,25 +12,20 @@ bg:'red'
 }
 };
 
-//  СЧЕТЧИК  КОЛ-ВА  СВОЙСТВ ВСЕХ  КАТЕГОРИЙ  И  ПОДКАТЕГОРИЙ  ОБЬЕКТА
+//  СЧЕТЧИК  КОЛ-ВА  СВОЙСТВ ВСЕХ  КАТЕГОРИЙ  БЕЗ  ПОДКАТЕГОРИЙ  ОБЬЕКТА ВЕРХНИЙ  УРОВЕНЬ
 let COUNTER = 0;
 for (let key in OPTIONS) {
-if (typeof(OPTIONS[key])  === 'object') {
-for (let i in OPTIONS[key]) {
-  console.log(`Свойство ${i} имеет значение ${OPTIONS[key][i]}`);
-  COUNTER++;
-}
+//if (typeof(OPTIONS[key])  === 'object') {
+//for (let i in OPTIONS[key]) {
+//   console.log(`Свойство ${i} имеет значение ${OPTIONS[key][i]}`);
+//   COUNTER++;
+// }
 
-} else {
-  console.log(`Свойство ${key} имеет значение ${OPTIONS[key]}`);
+// } else {
+//   console.log(`Свойство ${key} имеет значение ${OPTIONS[key]}`);
   COUNTER++;
-}
+//}
 }
 console.log(COUNTER);
-/* RUN CODE
-Свойство name имеет значение test
-Свойство width имеет значение 1024
-Свойство height имеет значение 1024
-Свойство border имеет значение black
-Свойство bg имеет значение red
-5   */
+/* RUN CODE --- > 4
+  */
