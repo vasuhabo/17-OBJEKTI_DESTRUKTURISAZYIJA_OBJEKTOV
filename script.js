e47f547f20aa9b731d63dd336a482221c1a298dd
sahumo <7523460@gmail.com>
2021-09-21 02:15:52 +0300

    Метод Object.keys()

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 
 "use strict";
+/*
+Метод Object.keys() возвращает массив из собственных перечисляемых свойств переданного объекта, в том же порядке, в котором они бы обходились циклом for...in (разница между циклом и методом в том, что цикл перечисляет свойства и из цепочки прототипов).
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+
+*/
+
 
 const OPTIONS = {
 name:'test',
@@ -12,9 +18,63 @@ bg:'red'
 }
 };
 
+console.log(Object.keys(OPTIONS));
+
+/* RUN CODE 
+
+[ 'name', 'width', 'height', 'colors' ]  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //  СЧЕТЧИК  КОЛ-ВА  СВОЙСТВ ВСЕХ  КАТЕГОРИЙ  БЕЗ  ПОДКАТЕГОРИЙ  ОБЬЕКТА ВЕРХНИЙ  УРОВЕНЬ
-let COUNTER = 0;
-for (let key in OPTIONS) {
+//let COUNTER = 0;
+//for (let key in OPTIONS) {
 //if (typeof(OPTIONS[key])  === 'object') {
 //for (let i in OPTIONS[key]) {
 //   console.log(`Свойство ${i} имеет значение ${OPTIONS[key][i]}`);
@@ -23,9 +83,9 @@ for (let key in OPTIONS) {
 
 // } else {
 //   console.log(`Свойство ${key} имеет значение ${OPTIONS[key]}`);
-  COUNTER++;
+  //COUNTER++;
 //}
-}
-console.log(COUNTER);
+///}
+//console.log(COUNTER);
 /* RUN CODE --- > 4
   */
