2cb73d6542db1d28ed3b0d2763949d56aaab67c6
sahumo <7523460@gmail.com>
2021-09-21 01:58:00 +0300

    СЧЕТЧИК  КОЛ-ВА  СВОЙСТВ ВСЕХ  КАТЕГОРИЙ  И  ПОДКАТЕГОРИЙ  ОБЬЕКТА

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 "use strict";
-// ВЫВОД ВСЕХ СВОЙСТВ И ПОДСВОЙСТВ ( ИЛИ  ПОДКАТЕГОРИЙ  ДАННЫХ ОБЬЕКТА)
+
 const OPTIONS = {
 name:'test',
 width:1024,
@@ -11,38 +11,26 @@ bg:'red'
 
 }
 };
-//console.log(OPTIONS["colors"]["border"]);
-// RUN CODE ---> black
 
-
-
-
-//delete OPTIONS.name;
-
-//console.log(OPTIONS);
-// ВЫВОД ВСЕХ СВОЙСТВ И ПОДСВОЙСТВ ( ИЛИ  ПОДКАТЕГОРИЙ  ДАННЫХ ОБЬЕКТА)
+//  СЧЕТЧИК  КОЛ-ВА  СВОЙСТВ ВСЕХ  КАТЕГОРИЙ  И  ПОДКАТЕГОРИЙ  ОБЬЕКТА
+let COUNTER = 0;
 for (let key in OPTIONS) {
 if (typeof(OPTIONS[key])  === 'object') {
 for (let i in OPTIONS[key]) {
   console.log(`Свойство ${i} имеет значение ${OPTIONS[key][i]}`);
+  COUNTER++;
 }
 
 } else {
   console.log(`Свойство ${key} имеет значение ${OPTIONS[key]}`);
+  COUNTER++;
 }
-
-
-
 }
-/* RUN CODE 
-
+console.log(COUNTER);
+/* RUN CODE
 Свойство name имеет значение test
 Свойство width имеет значение 1024
 Свойство height имеет значение 1024
 Свойство border имеет значение black
-Свойство bg имеет значение red */
-
-/*
-ССЫЛКА  НА  ВСЮ  ИНФОРМАЦИЮ  ПО  ОБЬЕКТУ
-
-https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object  */
+Свойство bg имеет значение red
+5   */
